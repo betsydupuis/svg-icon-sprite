62c8323a74e07eec94a17fc38ab997841153d391
Dupuis, Betsy <bdupuis@nextgen.com>
2015-11-30 15:00:43 -0600

    Create icon sprite build with grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,59 @@ module.exports = function (grunt) {
     // load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
     require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({});
-    grunt.registerTask('default', []);
+    grunt.initConfig({
+        clean: {
+            iconTemp: [ //removes temporary icons
+                    'dev/icons/cleaned'
+                    ],
+        },
+        svgmin: {
+            icons: {
+                options: {
+                    plugins: [
+                        { removeViewBox: true },
+                        { removeUselessStrokeAndFill: true},
+                        { mergePaths: false },
+                        { collapseGroups: true },
+                        { convertColors: false},
+                        { convertStyleToAttrs: false},
+                        { removeFill: true},
+                        { cleanupEnableBackground: false},
+                        { removeStyleElement: true}
+                    ],
+                },
+
+                files: [{
+                    expand: true,
+                    cwd: 'dev/icons/',
+                    src: ['*.svg'],
+                    dest: 'dev/icons/cleaned',
+                }],
+            }
+        },
+        svgstore: {
+            icons : {
+                options: {
+                    prefix : 'icon-', // this will prefix each id attribute of <symbol>
+                    svg: {
+                        // viewbox: '0 0 100 100',
+                        style: 'width: 0; height: 0;',
+                    },
+                    includedemo: true,
+                    cleanup: ['fill', 'overflow', 'enablebackground'],
+                },
+                files: {
+                    'dist/icons/icon-sprite.svg'
+                    :
+                    ['dev/icons/cleaned/*.svg'],
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('icons', ['svgmin:icons',
+                                 'svgstore:icons',
+                                 'clean:iconTemp',
+                                ]);
+    // grunt.registerTask('default', []);
 };
